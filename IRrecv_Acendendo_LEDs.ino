b2931996ee28dbabb1d01c84865741340d7d92c7
Everton <41173101+Everton1997@users.noreply.github.com>
2018-07-22 11:00:56 -0300

    Exemplos 1
    Exemplos de Arduíno

---
+++ IRrecv_Acendendo_LEDs.ino
@@ -0,0 +1,55 @@
+
+#include <IRremote.h>
+int ledpin1 = 11;
+int ledpin2 = 12;
+int ledpin3 = 10;
+int RECV_PIN = 3;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+  pinMode(ledpin1, OUTPUT);
+  pinMode(ledpin2, OUTPUT);
+  pinMode(ledpin3, OUTPUT);
+
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, DEC);
+    irrecv.resume(); // Receive the next value
+  }
+  {
+    if(results.value == 16753245)  // Valor referente ao botão 1
+    digitalWrite(ledpin1, HIGH);
+  }
+  {
+    if(results.value == 16736925)  // Valor referente ao botao 2
+    digitalWrite(ledpin2, HIGH);
+    }
+    {
+    if(results.value == 16720605)  // Valor referente ao botão 4
+    digitalWrite(ledpin1, LOW);
+  }
+  {
+    if(results.value == 16712445)  // Valor referente ao botao 5
+    digitalWrite(ledpin2, LOW);
+    }
+     {
+    if(results.value == 16769565)  // Valor referente ao botão 3
+    digitalWrite(ledpin3, HIGH);
+  }
+  {
+    if(results.value == 16761405)  // Valor referente ao botao 6
+    digitalWrite(ledpin3, LOW);
+    }
+  delay(100);
+}
---
+++ Sensor_Distancia.ino
@@ -0,0 +1,73 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Ultrasonic.h>
+
+#define pino_trigger 4
+#define pino_echo 5
+
+const int buzzer = 13;
+int LED1 = 12;
+int LED2 = 11;
+int LED3 = 10;
+
+LiquidCrystal_I2C lcd (0x3F, 20 ,4);
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+void setup() {
+  // put your setup code here, to run once:
+lcd.begin();
+lcd.clear();
+
+pinMode(LED1, OUTPUT);
+pinMode(LED2, OUTPUT);
+pinMode(LED3, OUTPUT);
+pinMode(buzzer, OUTPUT);
+
+Serial.begin(9600);
+Serial.println("Lendo dados do sensor...");
+}
+
+void loop() {
+{
+float cmMsec;
+long microsec = ultrasonic.timing();
+cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+
+
+lcd.setCursor(4,0);
+lcd.print("Distancia:");
+lcd.setCursor(1,1);
+lcd.print(cmMsec);
+lcd.setCursor(8,1);
+lcd.print("Centimetros");
+
+ Serial.print("Distancia: ");
+  Serial.print(cmMsec);
+  Serial.println(" Centimetros");
+
+
+{
+if(cmMsec < 5.0)
+digitalWrite(LED1, HIGH);
+else
+  digitalWrite(LED1, LOW);
+if(cmMsec > 5.0 && cmMsec < 8.0)
+digitalWrite(LED2, HIGH);
+else
+digitalWrite(LED2, LOW);
+if(cmMsec > 8.0)
+digitalWrite(LED3, HIGH);
+else
+digitalWrite(LED3, LOW);
+}
+{
+  if(cmMsec < 5.0)
+  tone(buzzer, 1500);
+  else
+  noTone(buzzer);
+  }
+
+}
+delay(1500);
+}
+
